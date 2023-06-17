9ddca46b18e876f8b771c83329b46d3502e0b4d3
yunusyalcinkaya <yunusyalcinkaya52@gmail.com>
2023-06-17 20:27:08 +0300

    add extra examples method reference

--- LambdaExpressions/src/methodReferences/Driver.java
+++ LambdaExpressions/src/methodReferences/Driver.java
@@ -2,6 +2,7 @@ package methodReferences;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.DoubleBinaryOperator;
 
 public class Driver {
     public static void main(String[] args) {
@@ -10,10 +11,18 @@ public class Driver {
         list.add(4.0);
         list.add(1.0);
         list.add(3.0);
-
         // * consumer functional interface
         // ? list.forEach(item -> System.out.println(item));
         // * bir üstteki satır ile aynı işi yapar
+        list.forEach(System.out::println);
+
+        System.out.println("example 2");
+        // * DoubleBinaryOperator -> functional interface
+        DoubleBinaryOperator operator = Math::pow;
+        list.forEach(item-> System.out.println(operator.applyAsDouble(item,2)));
+
+        System.out.println("example 3");
+        list.forEach(Math::sqrt);
         list.forEach(System.out::println);
 
     }

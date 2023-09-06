e40e4930b6d1446618dd43f344dec1a9e67a64e0
YUNUS YALÇINKAYA <82769404+yunusyalcinkaya@users.noreply.github.com>
2023-09-06 20:02:30 +0300

    bug fix

--- Tests/src/ArrangeRedBalls.java
+++ Tests/src/ArrangeRedBalls.java
@@ -11,39 +11,51 @@ public class ArrangeRedBalls {
     */
 
     public static int solution(String s){
-        List<Integer> list = new ArrayList<>();
+        // index list of red balls
+        List<Integer> indexList = new ArrayList<>();
+        // transfer indexes of red balls to indexList
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c == 'R') list.add(i);
+            if(c == 'R') indexList.add(i);
         }
-        int size = list.size();
-        int average = size % 2 == 0
-                ? (list.get((int) size / 2) + (list.get((int) size / 2 -1))) / 2
-                : list.get((int) size / 2);
 
-        int sum = list.stream()
-                .map(item -> Math.abs(item - average) - 1)
+        int sizeOfIndexList = indexList.size();
+        int halfSizeOfIndexList = (int)sizeOfIndexList /2;
+        if(sizeOfIndexList == 0 || sizeOfIndexList == s.length()) return 0;
+
+        int midOfIndexList = sizeOfIndexList % 2 == 0
+                ? (indexList.get(halfSizeOfIndexList) + (indexList.get(halfSizeOfIndexList -1))) / 2
+                : indexList.get(halfSizeOfIndexList);
+
+        int sum = indexList.stream()
+                .map(item -> Math.abs(item - midOfIndexList))
                 .mapToInt(Integer::intValue)
-                .sum() + 1;
-        return size % 2==0 && size > 2 ? sum -1 : sum;
+                .sum();
+
+        sum -= sizeOfIndexList % 2 == 0
+                ? Math.pow(halfSizeOfIndexList,2)
+                : halfSizeOfIndexList * (halfSizeOfIndexList +1);
+
+        return sum;
     }
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println(solution("WWR")); //! 0
-        System.out.println(solution("WWRWWWRWR")); //! 4
-        System.out.println(solution("WRRWWR")); //! 2
-        System.out.println(solution("RWWRR")); //! 2
-        System.out.println(solution("RWWWWR")); //! 4
-        System.out.println(solution("RWRWR")); //! 2
-        System.out.println(solution("WRWRR")); //! 1
-        System.out.println(solution("WRWWRR")); //! 2
-        System.out.println(solution("RRWWRW")); //! 2
-        System.out.println(solution("RWWWWWWWRR")); //! 7
-        System.out.println(solution("WRRWWR")); //! 2
-        System.out.println(solution("WWRWWWRWR")); //! 4
-        System.out.println(solution("RWWRWWWRWR")); //! 9
-        System.out.println(solution("WRWWRRWWR")); //! 4
-        System.out.println(solution("RWWRWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWRR")); //! 84
+        System.out.println("0  -> " + solution("W"));
+        System.out.println("0  -> " + solution("WWWWWWW"));
+        System.out.println("0  -> " + solution("R"));
+        System.out.println("0  -> " + solution("RRRRRRR"));
+        System.out.println("0  -> " + solution("WWR"));
+        System.out.println("4  -> " + solution("WWRWWWRWR"));
+        System.out.println("2  -> " + solution("WRRWWR"));
+        System.out.println("2  -> " + solution("RWWRR"));
+        System.out.println("4  -> " + solution("RWWWWR"));
+        System.out.println("2  -> " + solution("WRWWRR"));
+        System.out.println("2  -> " + solution("RRWWRW"));
+        System.out.println("9  -> " + solution("RWWRWWWRWR"));
+        System.out.println("4  -> " + solution("WRWWRRWWR"));
+        System.out.println("84 -> " + solution("RWWRWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWRR"));
+        System.out.println("3  -> " + solution("WRWWRRRWR"));
+
     }
 }

a8295bafcf0b829f0e5c6fc32293a98f387b3412
YUNUS YALÇINKAYA <82769404+yunusyalcinkaya@users.noreply.github.com>
2023-09-08 00:13:40 +0300

    code review

--- Tests/src/ArrangeRedBalls.java
+++ Tests/src/ArrangeRedBalls.java
@@ -13,7 +13,7 @@ public class ArrangeRedBalls {
     public static int solution(String s){
         // index list of red balls
         List<Integer> indexList = new ArrayList<>();
-        // transfer indexes of red balls to indexList
+        // add indexes of red balls to indexList
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             if(c == 'R') indexList.add(i);
@@ -22,7 +22,7 @@ public class ArrangeRedBalls {
         int sizeOfIndexList = indexList.size();
         int halfSizeOfIndexList = (int)sizeOfIndexList /2;
         if(sizeOfIndexList == 0 || sizeOfIndexList == s.length()) return 0;
-
+            // 2 6 8
         int midOfIndexList = sizeOfIndexList % 2 == 0
                 ? (indexList.get(halfSizeOfIndexList) + (indexList.get(halfSizeOfIndexList -1))) / 2
                 : indexList.get(halfSizeOfIndexList);
@@ -33,11 +33,17 @@ public class ArrangeRedBalls {
                 .sum();
 
         sum -= sizeOfIndexList % 2 == 0
-                ? Math.pow(halfSizeOfIndexList,2)
+                ? Math.pow(halfSizeOfIndexList,2) // n*(n+1)/2 + n*(n-1)/2 = n^2
                 : halfSizeOfIndexList * (halfSizeOfIndexList +1);
 
         return sum;
     }
+
+    /*
+            R R R R W R  -> 1
+            0 1 2 3   5 -> 11, 10 çıkarmam lazım, n=4, n*(n+1)/2 = 10
+              0 0 0   1
+     */
 
     public static void main(String[] args) throws IOException {
 
--- Tests/src/CaesarCipher.java
+++ Tests/src/CaesarCipher.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CaesarCipher {
     /*
@@ -6,32 +7,32 @@ public class CaesarCipher {
     ? Özel karakterler aynı kalsın. Yeni string'i döndür
      */
     //  https://www.hackerrank.com/challenges/caesar-cipher-1/problem?isFullScreen=true
-    // * A-Z -> 65-90, a-z -> 97-122
 
-    public static String caesarCipher(String s, int k){
+    private static String caesarCipher2(String s, int k){
 
-        char[] arr = s.toCharArray();
+        String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        String upperAlphabet = lowerAlphabet.toUpperCase();
+
         k %= 26;
-        for(int i=0; i<arr.length; i++){
-            if(((int)arr[i] >= 65 && (int)arr[i] <= 90)){
-                if((int)arr[i] + k >90){
-                    arr[i] = (char)((((int)arr[i] +k) %91) +65);
-                }
-                else arr[i] = (char)((int)arr[i] +k);
-            }
-            else if ((int)arr[i] >= 97 && (int)arr[i] <= 122) {
-                if((int)arr[i] +k >122){
-                    arr[i] = (char)((((int)arr[i] +k) %123) +97);
-                }
-                else arr[i] = (char)((int)arr[i] +k);
-            }
+        char[] array = s.toCharArray();
+
+        int index = 0;
+        for(int i=0; i<array.length; i++){
+
+            array[i] = lowerAlphabet.indexOf(array[i]) != -1
+                    ? lowerAlphabet.charAt((lowerAlphabet.indexOf(array[i]) +k) %26)
+                    : array[i];
+
+            array[i] = upperAlphabet.indexOf(array[i]) != -1
+                    ? upperAlphabet.charAt((upperAlphabet.indexOf(array[i]) +k) %26)
+                    : array[i];
         }
-        return new String(arr);
+        return String.valueOf(array);
     }
 
     public static void main(String[] args) {
-        System.out.println(caesarCipher("abc.,-",2));
-        System.out.println(caesarCipher("abC.,-",3));
-        System.out.println(caesarCipher("xyz.,-",4));
+        System.out.println(caesarCipher2("abc.,-",2));
+        System.out.println(caesarCipher2("abC.,-",3));
+        System.out.println(caesarCipher2("xyz.,-",4));
     }
 }
